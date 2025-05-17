3b2d582f4d883d5a1906eb7a7d92099ffa6729ac
DayeneS <dayene-santos@hotmail.com>
2025-05-16 21:23:39 -0300

    AgendaService criada: adiciona, lista, busca, remove e conclui tarefas

---
+++ iniciante/agenda-pessoal/src/main/java/br/com/dayene/agendapessoal/service/AgendaService.java
@@ -0,0 +1,48 @@
+package br.com.dayene.agendapessoal.service;
+
+import br.com.dayene.agendapessoal.model.Tarefa;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class AgendaService {
+    private List<Tarefa> tarefas = new ArrayList<>();
+    private int proximoId = 1; // Garante que cada tarefa tenha um ID único
+
+    public void adicionarTarefa(String titulo, String descricao, String data, String prioridade) {
+        Tarefa novaTarefa = new Tarefa(proximoId, titulo, descricao, data, prioridade);
+        tarefas.add(novaTarefa);
+        proximoId++;
+    }
+
+    public List<Tarefa> listarTarefas() {
+        return tarefas;
+    }
+
+    public Tarefa buscarPorId(int id) {
+        for (Tarefa tarefa : tarefas) {
+            if (tarefa.getId() == id) {
+                return tarefa;
+            }
+        }
+        return null; // Não encontrada
+    }
+
+    public boolean removerTarefa(int id) {
+        Tarefa tarefa = buscarPorId(id);
+        if (tarefa != null) {
+            return tarefas.remove(tarefa);
+        }
+        return false;
+    }
+    public boolean concluirTarefa(int id) {
+        Tarefa tarefa = buscarPorId(id);
+        if (tarefa != null && !tarefa.isConcluida()) {
+            tarefa.marcarComoConcluida();
+            return true;
+        }
+        return false;
+    }
+
+
+}
